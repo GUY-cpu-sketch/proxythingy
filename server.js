95940c6e5b12879a8340b167aaf679c3b538f4b5
GUY-cpu-sketch <masonewyatt1@gmail.com>
2025-09-24 12:45:55 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,26 +2,29 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 
-// Serve frontend
 app.use(express.static('public'));
 
 // Proxy endpoint
 app.get('/proxy', async (req, res) => {
-  const target = 'https://www.blooket.com/'; // site to proxy
+  const target = 'https://www.blooket.com/';
   try {
     const response = await axios.get(target);
     let html = response.data;
 
-    // Inject custom JS
+    // Inject a script to allow receiving JS commands from parent
     const inject = `
       <script>
-        console.log('Injected JS works!');
-        window.alert('You can run JS here!');
+        window.addEventListener('message', e => {
+          try {
+            const result = eval(e.data);
+            e.source.postMessage({ type: 'result', data: result }, e.origin);
+          } catch(err) {
+            e.source.postMessage({ type: 'error', data: err.toString() }, e.origin);
+          }
+        });
       </script>
     `;
     html = html.replace('</body>', inject + '</body>');
-
-    // Serve rewritten page
     res.send(html);
   } catch (err) {
     res.status(500).send('Error fetching target site.');
